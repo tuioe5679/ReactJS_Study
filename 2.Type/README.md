7812129b66f7be91c0bfb0ed63900f33d73f7836
tuioe <qodlgks123@naver.com>
2024-05-19 05:50:34 +0000

    자료형 분류 및 설명

---
+++ 2.Type/README.md
@@ -0,0 +1,9 @@
+### 자료형(Type) = 집합 
+```동일한 속성이나 특징을 가지는 원소들을 묶어 놓은 것```
+
+#### 원시타입 
+- Number 
+- String
+- Boolean
+- Null
+- Undefined 
---
+++ 2.Type/index_2.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num = 10;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+console.log(1 * "Hello"); // 연산값 = NaN 
+
+// 2.String Type 
+let myName = "name";
+let location = "위치";
+let introduce = myName + location;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${location}에 있다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type = 사용자가 직접 아무것도 없는 값이라고 지정 
+let empty = null;
+
+// 5. Undefined Type = 변수를 선언하고 초기화를 하지 않으면 자동으로 지정
+let none; 
