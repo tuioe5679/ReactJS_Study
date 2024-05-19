15f550cbfa3befad85a90fd66187f11ce46db557
tuioe <qodlgks123@naver.com>
2024-05-19 05:40:10 +0000

    변수와 상수

---
+++ 1.JS/index_1.js
@@ -0,0 +1,20 @@
+// 변수
+
+let age = 10
+
+// 변수 명명규칙(네이밍)
+
+// 1. $, _ 제외한 기호는 사용할 수 없다
+// 2. 숫자로 시작할 수 없다  
+// 3. 예약어는 사용할 수 없다
+
+// 변수 이름 가이드 
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
+
+// 상수 
+const birth = "123"
+
+console.log(age)
+console.log(birth)
