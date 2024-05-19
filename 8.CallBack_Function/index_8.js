47b1b3983a9cd46c32a1a066f71873e3deab8cfb
tuioe <qodlgks123@naver.com>
2024-05-19 07:03:45 +0000

    콜백 함수

---
+++ 8.CallBack_Function/index_8.js
@@ -0,0 +1,35 @@
+// 1. 콜백함수 
+function main(value){
+    console.log(1);
+    console.log(2);
+    value();
+    console.log("end");
+}
+
+main(() => {
+    console.log("sub");
+});
+
+// 2. 콜백함수 활용
+function repeat(count,callback){
+    for(let idx = 1;idx <= count; idx++){
+        callback(idx);
+    }
+}
+
+repeat(5, (idx) => {
+    console.log(idx);
+});
+
+repeat(5,(idx) => {
+    console.log(idx * 2);
+});
+
+/*function repeatDouble(count){
+    for(let idx = 1;idx <= count; idx++){
+        console.log(idx * 2);
+    }
+}*/
+
+repeat(5);
+repeatDouble(5);
